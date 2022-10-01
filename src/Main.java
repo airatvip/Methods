d86ce9d1f92fb0932731baa3bd2f353595fe9b29
Айрат <airat_vip@mail.ru>
2022-10-01 11:47:54 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 2020;
+        int clientDeviceYear = 2014;
+        int clientOS = 0;
+        int deliveryDistance = 95;
+
+        printYear(year);
+        installationApp(clientDeviceYear, clientOS);
+        System.out.println("Потребуетя дней: " + deliveryTime(deliveryDistance));
+
     }
+
+
+    public static void printYear(int year) {
+        if (year % 4 == 0 && year % 100 > 0) {
+            System.out.println(year + " год является високосным ");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " год является високосным ");
+        } else {
+            System.out.println(year + " год не является високосным ");
+        }
+    }
+
+    public static void installationApp(int clientDeviceYear, int clientOS) {
+
+
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        }
+    }
+
+    public static int deliveryTime(int deliveryDistance) {
+        int deliveryTime = 1;
+        if (deliveryDistance <= 20) {
+            return deliveryTime;
+        } else {
+            deliveryTime = ((deliveryDistance - 20) / 40) * deliveryTime + 2;
+            return deliveryTime;
+        }
+
+    }
+
+
 }
+
